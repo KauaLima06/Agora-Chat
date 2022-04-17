e78bbaa77967e8fe788733ca83292e945561c45f
Kauã Lima <lima.kaua2006@gmail.com>
2022-04-16 21:30:16 -0300

    Lading page almost finished, missing only the section tools

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+//Open and Close Menu Side Bar
+const menuIcon = document.getElementById('menuIcon');
+const menuSideBar = document.querySelector('.menuSideBarClose');
+menuIcon.addEventListener('click', () =>{
+    if(menuIcon.name == 'menu'){
+        menuSideBar.classList.remove('menuSideBarClose');
+        menuSideBar.classList.add('menuSideBarOpen');
+        menuIcon.name = 'close';
+    } else if(menuIcon.name == 'close'){
+        menuSideBar.classList.remove('menuSideBarOpen');
+        menuSideBar.classList.add('menuSideBarClose');
+        menuIcon.name = 'menu';   
+    }else{
+        return
+    }
+});
+//Open and Close Menu Side Bar
