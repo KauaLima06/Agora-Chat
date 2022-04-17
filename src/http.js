92c84ee8161b881282312d740ddca4818ae0a242
Kauã Lima <lima.kaua2006@gmail.com>
2022-04-17 08:09:43 -0300

    create http server

---
+++ src/http.js
@@ -0,0 +1,32 @@
+//Modules
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+const routers = require('../routers/routers.js');
+const bodyParse = require('body-parser');
+const e = require('cors');
+//Modules
+
+const app = express();
+
+//App configs
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../public/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(bodyParse.urlencoded({extended:false}));
+app.use(bodyParse.json());
+//App configs
+
+//Routers
+app.use('/', routers);
+//Routers
+
+//Server
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp);
+//Server
+
+//Export
+module.exports = { serverHttp , io };
