ee20e29e50d76f57dfdca8c8054ba02636d29464
Kauã Lima <lima.kaua2006@gmail.com>
2022-04-17 08:37:35 -0300

    create procfile

--- routers/routers.js
+++ routers/routers.js
@@ -5,19 +5,6 @@ require('body-parser');
 
 //Routers
 
-//Get routers
-router.get('/', (req, res) => {
-    res.render('index.html');
-});
-router.get('/login.html', (req, res) => {
-    res.render('login.html')
-});
-//Get routers
-
-//Post routers
-
-//Post routers
-
 //Routers
 
 module.exports = router;
